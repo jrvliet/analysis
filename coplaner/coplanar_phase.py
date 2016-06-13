7ae309226721d7ae94ecc0dcf982d50abe4a5977
Jacob Vander Vliet <jvander@g2.hpc.swin.edu.au>
2016-06-14 09:49:58 +1000

    Added column of total plots and a check on EW cut

--- coplaner/coplanar_phase.py
+++ coplaner/coplanar_phase.py
@@ -15,15 +15,14 @@ def plot_hist(x, y, numbins, binrange, ax, label):
 
     h, xedges, yedges = np.histogram2d( x, y, bins=numbins, range=binrange)
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-    print h.max()
 
     h = np.rot90(h)
     h = np.flipud(h)
     h = np.ma.masked_where(h==0, h)
 
+    print label, np.sum(h)
     h = np.log10(h)
 
-    print h.max()
     # Generate the plots
     cmap = plt.cm.get_cmap('viridis')
     mesh = ax.pcolormesh(xedges, yedges, h, cmap=cmap)
@@ -34,8 +33,9 @@ def plot_hist(x, y, numbins, binrange, ax, label):
     ax.set_title(label, fontsize=10)
     
     cbar = plt.colorbar(mesh, ax=ax, use_gridspec=True)
+    cbarLabel = 'Counts'
     cbar.ax.get_yaxis().labelpad = 20
-    #cbar.ax.set_ylabel(cbarLabel, rotation=270, fontsize=12)
+    cbar.ax.set_ylabel(cbarLabel, rotation=270, fontsize=12)
 
 
 def convert_angle(phi):
@@ -59,20 +59,30 @@ def convert_angle(phi):
 
 
 coplanarCut = 20   # PA below which the cells are considered coplanar
-perpCut = 60       # PA below which the cells are considered coplanar
-impactCut = 0.1    # Impact papameter below which it will be considered ISM [Rvir]
+perpCut = 45       # PA above which the cells are considered outflow
+impactCut = 0.2    # Impact papameter below which it will be considered ISM [Rvir]
+ewCut = 0.1        # EW cut [A]
+
+planeSize = coplanarCut
+voidSize = perpCut - coplanarCut
+minorSize = 90 - perpCut
+
 numbins = 50
 binrange = [[-8,1],[2,8]]
 
 ions = ['MgII','CIV', 'OVI']
+galID = 28
+expn = '0.490'
+inc = 90
 
 numions = len(ions)
-fig, axes = plt.subplots(numions, 3, figsize=(10,10))
+fig, axes = plt.subplots(numions, 4, figsize=(12,9))
 
 for ion, axrow in zip(ions, axes):
-    testloc = '/mnt/cluster/abs/cgm/vela2b/vela28/a0.490/i90/{0:s}/'.format(ion)
-    testfile = 'vela2b-28.0.490.{0:s}.i90.abs_cells.h5'.format(ion)
-    filename = '{0:s}/{1:s}'.format(testloc,testfile)
+    testloc = '/mnt/cluster/abs/cgm/vela2b/vela{0:d}/a{1:s}/i{2:d}/{3:s}/'.format(galID, expn, inc,ion)
+    absfile = 'vela2b-{0:d}.{1:s}.{2:s}.i{3:d}.abs_cells.h5'.format(galID, expn, ion, inc)
+    sysfile = 'vela2b-{0:d}.{1:s}.a{2:s}.ALL.sysabs.h5'.format(galID,ion,expn)
+    filename = '{0:s}/{1:s}'.format(testloc,absfile)
 
     # Get the virial radius
     with open(testloc+'../galaxy.props', 'r') as f:
@@ -80,9 +90,11 @@ for ion, axrow in zip(ions, axes):
             line = f.readline()
         rvir = float(line.split()[1])
 
-    # Read in the file
+    # Read in the files
     d = pd.read_hdf(filename, 'data')
     numcells = d.shape[0]
+    dsys = pd.read_hdf(testloc+sysfile, 'data')
+    ew = dsys['EW_r']
 
     # Read in lines information
     losnum, b, phi = np.loadtxt(testloc+'lines.info', skiprows=1, usecols=(0,1,2), unpack=True)
@@ -94,6 +106,7 @@ for ion, axrow in zip(ions, axes):
     coDense, coTemp = [], []
     nonDense, nonTemp = [], []
     perpDense, perpTemp = [], []
+    dense, temp = [], []
 
     # Loop over the absorption cells
     for i in range(1, numcells):
@@ -101,8 +114,9 @@ for ion, axrow in zip(ions, axes):
         index = int(d['LOS'][i]) - 1
         angle = pa[index]
         impact = b[index]
+        width = ew[index]
         
-        if impact > 0.1*rvir:
+        if impact > impactCut*rvir and width>=ewCut:
             if angle < coplanarCut:
                 coDense.append( d['log_nH'][i] )
                 coTemp.append( d['log_T'][i] )
@@ -112,6 +126,8 @@ for ion, axrow in zip(ions, axes):
             else:
                 nonDense.append( d['log_nH'][i] )
                 nonTemp.append( d['log_T'][i] )
+            dense.append( d['log_nH'][i])
+            temp.append( d['log_T'][i])
             
     # Bin the data and plot
     label = '{0:s} Within {1:d} deg of Major Axis'.format(ion, coplanarCut)
@@ -123,9 +139,12 @@ for ion, axrow in zip(ions, axes):
     label = '{0:s} Within {1:d} deg of Minor Axis'.format(ion, 90-perpCut)
     plot_hist(perpDense, perpTemp, numbins, binrange, axrow[2], label)
 
+    label = '{0:s} All'.format(ion, 90-perpCut)
+    plot_hist(dense, temp, numbins, binrange, axrow[3], label)
+
 
 fig.tight_layout()
-s = 'vela2b-28_a0.490_i90_EWcut100mA.pdf'
+s = 'vela2b-{0:d}_a{1:s}_i{2:d}_EWcut{3:d}mA_PAphase.pdf'.format(galID,expn,inc,int(ewCut*1000))
 plt.savefig(s, bbox_inches='tight')
 
 

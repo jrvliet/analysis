Fixed error so now converts angle to radians before trig function

positionAngle/pa.py
@@ -15,13 +15,15 @@ def calc_pa(phi):
     Calculates the position angle for the LOS
     '''
     if phi<=90:
-        return phi
+        val = phi
     elif phi>90 and phi<=180:
-        return 180.0 - phi
+        val = 180.0 - phi
     elif phi>180 and phi<=270:
-        return phi - 180.0
+        val = phi - 180.0
     else:
-        return 350.0 - phi
+        val = 360.0 - phi
+
+    return val*np.pi/180.0
 
 def gmean(vals):
     '''
@@ -32,10 +34,11 @@ def gmean(vals):
 coplaneCut = np.radians(20)
 outflowCut = np.radians(60)
 numbins = 50
+ewcut = 0.1
 
 location = '/mnt/cluster/abs/cgm/vela2b/'
 halo_list = [25, 26, 27, 28]
-halo_list = [28]
+halo_list = [27, 28]
 expn = '0.490'
 incline = 90
 ions = ['HI', 'MgII', 'CIV', 'OVI']
@@ -58,49 +61,68 @@ for galNum in halo_list:
         absfile = absfileBase.format(galNum,expn,ion,incline)
         sysabsfile = sysabsfileBase.format(galNum,ion,expn)
 
+        # Get Rvir
+        with open(loc+'../galaxy.props', 'r') as f:
+            for i in range(5):
+                line = f.readline()
+            rvir = float(line.split()[1])
+
         dcell = pd.read_hdf(loc+absfile, 'data')
         dabs = pd.read_hdf(loc+sysabsfile, 'data')
 
+        print loc
+        print sysabsfile
         phi = []
         for p in dabs['phi']:
             phi.append(calc_pa(p))
 
 
-        ew = dabs['EW_r'][ dabs['EW_r']>0 ]
+        b = dabs['D']
+        
+        ew, x, y = [], [], []
+        for i in range(len(dabs)):
+            if dabs['EW_r'][i]>=ewcut:
+                ew.append(dabs['EW_r'][i])
+                x.append(b[i] * np.cos(phi[i]))
+                y.append(b[i] * np.sin(phi[i]))
+
+        #ew = dabs['EW_r'][ dabs['EW_r']>ewcut ]
         logN = dabs['logN'][ dabs['logN']>0 ]
 
-        b = dabs['D']
-
-        x = [ i*np.cos(j) for i,j in zip(b,phi)]
-        y = [ i*np.sin(j) for i,j in zip(b,phi)]
+        #x = [ i*np.cos(j) for i,j in zip(b,phi)]
+        #y = [ i*np.sin(j) for i,j in zip(b,phi)]
     
         # Make 2d histogram of x, y position on sky, with bin value
-        bin_means, x_edges, y_edges, binnumber = stats.binned_statistic_2d(x,y,dabs['EW_r'],
+        bin_means, x_edges, y_edges, binnumber = stats.binned_statistic_2d(x,y,ew,
                 statistic='mean', bins=numbins)
+        bin_means = np.rot90(bin_means)
+        bin_means = np.flipud(bin_means)
         whereNans = np.isnan(bin_means)
         bin_means[whereNans] = 0.0
         bin_means = np.ma.masked_where(bin_means==0, bin_means)
         bin_means = np.log10(bin_means)
         mesh = ax.pcolormesh(x_edges, y_edges, bin_means)
-        ax.set_xlim([0,x_edges[-1]])
-        ax.set_ylim([0,y_edges[-1]])
+        ax.set_xlim([0,1.5*rvir])
+        ax.set_ylim([0,1.5*rvir])
+        ax.set_xlabel('Major Axis')
+        ax.set_ylabel('Minor Axis')
         ax.set_title(ion)
         cbar = fig.colorbar(mesh, ax=ax, use_gridspec=True)
         cbar.ax.get_yaxis().labelpad = 20
         cbar.ax.set_ylabel('Mean EW', rotation=270, fontsize=12)
 
         # Draw lines seperating out the coplanar regions and the outflow regions
-        x = [0, x_edges[-1]*np.cos(coplaneCut)]
-        y = [0, x_edges[-1]*np.sin(coplaneCut)]
+        x = [0, 1.5*rvir*np.cos(coplaneCut)]
+        y = [0, 1.5*rvir*np.sin(coplaneCut)]
         ax.plot(x,y,linestyle='dotted',color='k')
-        x = [0, x_edges[-1]*np.cos(outflowCut)]
-        y = [0, x_edges[-1]*np.sin(outflowCut)]
+        x = [0, 1.5*rvir*np.cos(outflowCut)]
+        y = [0, 1.5*rvir*np.sin(outflowCut)]
         ax.plot(x,y,linestyle='dotted',color='k')
 
 
 
     fig.tight_layout()
-    fig.savefig('pahist_vela2b-{0:d}_ew_{1:s}.png'.format(galNum, statname), bbox_inches='tight')
+    fig.savefig('pahist_vela2b-{0:d}_ew_{1:s}_ewcut{2:d}mA.pdf'.format(galNum, statname, int(1000*ewcut)), bbox_inches='tight')
 
 
 
9cd81ef82e899d49d3e3e6e118b773990555ae87
Jacob Vander Vliet <jvander@g2.hpc.swin.edu.au>
2016-09-02 07:16:39 +1000

    Radial velocity now fully tested

--- inflows/inflow_radial_velocity.py
+++ inflows/inflow_radial_velocity.py
@@ -9,22 +9,32 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 fname = '/mnt/cluster/abs/cgm/vela2b/vela27/a{0:.3f}/vela2b-27_GZa{0:.3f}.h5'
+rname = '/mnt/cluster/abs/cgm/vela2b/vela27/a{0:.3f}/rotmat_a{0:.3f}.txt'
 
 
 loT, hiT = 10**3, 10**4.5
 loN, hiN = 10**-6, 10**-2.25
 
-
 expns = np.arange(0.200,0.510,0.01)
+
+minv, maxv = [],[]
 
 for a in expns:
 
     z = 1./a - 1.0
 
+    with open(rname.format(a),'r') as f:
+        f.readline()
+        rvir = float( f.readline().split()[3] ) 
+
     fn = fname.format(a)
 
-    df = pd.read_hdf(fn,'data')
+    try:
+        df = pd.read_hdf(fn,'data')
+    except IOError:
+        continue
 
     index = ( (df['temperature']<hiT) & (df['temperature']>loT) &
                 (df['density']<hiN) & (df['density']>loN) & 
@@ -36,9 +46,20 @@ for a in expns:
     cloud['vr'] = ( cloud['vx']*cloud['x'] + cloud['vy']*cloud['y'] + 
                     cloud['vz']*cloud['z'] ) / cloud['r']
 
+    posInds = (cloud['vr']>0)
+    negInds = (cloud['vr']<0)
+    poscloud = cloud[posInds]
+    negcloud = cloud[negInds]
+    minv.append(cloud['vr'].min())
+    maxv.append(cloud['vr'].max())
+    print(cloud['r'].max()/rvir)
     
     fig, ax = plt.subplots(1,1,figsize=(10,10))
-    ax.plot(cloud['r'], cloud['vr'], 'ok', alpha=0.01)
+    
+    ax.plot(poscloud['r']/rvir, poscloud['vr'], 'ob', alpha=0.01)
+    ax.plot(negcloud['r']/rvir, negcloud['vr'], 'or', alpha=0.01)
+    ax.set_xlim([0,5.0])
+    ax.set_ylim([-500,500])
     ax.set_xlabel('Galactocentric Distance [kpc]')
     ax.set_ylabel('Radial Velocity [km/s]')
     ax.set_title('z = {0:.3f}'.format(z))
@@ -48,6 +69,8 @@ for a in expns:
 
     
     
+print( 'Min Rad Vel = {0:.2f}'.format(min(minv)))
+print( 'Max Rad Vel = {0:.2f}'.format(max(maxv)))
 
 
 

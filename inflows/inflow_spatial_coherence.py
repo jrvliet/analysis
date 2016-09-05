9ce9e04887f0130208f9ba97974265a025fc3172
Jacob Vander Vliet <jrvliet@gmail.com>
2016-09-04 19:32:02 -0600

    Spatial coherence ready for testing

--- inflows/inflow_spatial_coherence.py
+++ inflows/inflow_spatial_coherence.py
@@ -2,7 +2,8 @@
 '''
 Determines the coherence of the inflows
 Focus on the poster child inflow in vela2b-27
-at a=0.490
+Outputs variance of data and scale of fit 
+for all times
 '''
 
 
@@ -17,135 +18,89 @@ import scipy.linalg as sl
 import scipy.stats as st
 
 pd.options.mode.chained_assignment = None
+plotting = 0
 
 loT, hiT = 10**3.25, 10**4.5
 loN, hiN = 10**-6.25, 10**-2.25
 numbins = 200
 
-dataloc = '/mnt/cluster/abs/cgm/vela2b/vela27/a0.490/'
-dataloc = '/home/jacob/research/velas/vela2b/vela27/a0.490/'
-fname = '{0:s}vela2b-27_GZa0.490.h5'.format(dataloc)
+dataloc = '/mnt/cluster/abs/cgm/vela2b/vela27/a{0:.3f}/'
+dataloc = '/home/jacob/research/velas/vela2b/vela27/a{0:.3f}/'
 
-df = pd.read_hdf(fname, 'data')
+expns = np.arange(0.200,0.500,0.01)
 
-index = ( (df['temperature']>loT) & (df['temperature']<hiT) & 
-            (df['density']>loN) & (df['density']<hiN) &
-            (df['x']>0) & (df['z']>0) & (np.abs(df['y'])<300) )
+outfilename = 'vela2b-27_cloud1_rayleightFit.dat'
+fout = open(outfilename, 'w')
+header = 'Expn\tNumber\tScale\tLoc\tRayVary\tDataVar\tKS\n'
+fout.write(header)
+form = '{0:.3f}\t{1:d}\t{2:.3f}\t{3:.3f}\t{4:.3f}\t{5:.3f}\t{6:.3f}\n'
 
-cloud = df[index]
-cloudLoc = cloud[['x','y','z']]
-print('Number of samples = {0:d}'.format(len(cloudLoc)))
+for a in expns:
 
-# Perform a PCA to find the line of best fit
+    fname = '{0:s}vela2b-27_GZa{1:s}.h5'.format(dataloc,a)
 
-# Start by normalizing the cells
-locM = cloudLoc - cloudLoc.mean()
+    df = pd.read_hdf(fname, 'data')
 
-# Get the covariance matrix
-covar = locM.cov()
+    index = ( (df['temperature']>loT) & (df['temperature']<hiT) & 
+                (df['density']>loN) & (df['density']<hiN) &
+                (df['x']>0) & (df['z']>0) & (np.abs(df['y'])<300) )
 
-# Determine the single value decomposition of the covariance matrix
-(u,s,v) = sl.svd(covar)
+    cloud = df[index]
+    cloudLoc = cloud[['x','y','z']]
+    numCells = len(cloudLoc)
 
-# The first column of u is the directional vector of the line
-# of best fit for the cloud
-u0 = u[0,0]
-u1 = u[1,0]
-u2 = u[2,0]
+    # Perform a PCA to find the line of best fit
 
-# Determine the distance from each cell in the cloud
-# to the this line
-locM['a0'] = u2*locM['y'] - u1*locM['z']
-locM['a1'] = u0*locM['z'] - u2*locM['x']
-locM['a2'] = u1*locM['x'] - u0*locM['y']
-locM['dist'] = np.sqrt(locM['a0']**2 + locM['a1']**2 + locM['a2']**2)
+    # Start by normalizing the cells
+    locM = cloudLoc - cloudLoc.mean()
 
-out = pd.cut(locM['dist'], bins=numbins)
-counts = pd.value_counts(out, sort=False)
-counts.to_csv('counts.out',sep=' ',index=True,header=False)
-normedCounts = counts/sum(counts)
+    # Get the covariance matrix
+    covar = locM.cov()
 
-# Plot historgram of dist
-fig, (ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
-n, bins, patches = ax1.hist(locM['dist'], bins=numbins, 
-                            range=[locM['dist'].min(),locM['dist'].max()],
-                            histtype='step',normed=True)
-n, bins, patches = ax2.hist(locM['dist'], bins=numbins, 
-                            range=[locM['dist'].min(),locM['dist'].max()],
-                            histtype='step',normed=True,cumulative=True)
+    # Determine the single value decomposition of the covariance matrix
+    (u,s,v) = sl.svd(covar)
 
-#distNames = [ 'weibull_max', 'rayleigh']
-#for distName in distNames:
-#    print(distName)
-#    dist = getattr(st, distName)
-#    param = dist.fit(y)
-#    print(param)
-#    pdfFitted = dist.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
+    # The first column of u is the directional vector of the line
+    # of best fit for the cloud
+    u0 = u[0,0]
+    u1 = u[1,0]
+    u2 = u[2,0]
 
+    # Determine the distance from each cell in the cloud
+    # to the this line
+    locM['a0'] = u2*locM['y'] - u1*locM['z']
+    locM['a1'] = u0*locM['z'] - u2*locM['x']
+    locM['a2'] = u1*locM['x'] - u0*locM['y']
+    locM['dist'] = np.sqrt(locM['a0']**2 + locM['a1']**2 + locM['a2']**2)
 
-# Fit a rayleigh curve to the data
-y = locM['dist']
+    out = pd.cut(locM['dist'], bins=numbins)
+    counts = pd.value_counts(out, sort=False)
+    counts.to_csv('counts.out',sep=' ',index=True,header=False)
+    normedCounts = counts/sum(counts)
 
-# Perform fits and 
-x = np.linspace(y.min(), y.max(), 10000)
+    # Fit a rayleigh curve to the data
+    y = locM['dist']
 
-print('\nRayleigh Distribution')
-param = st.rayleigh.fit(locM['dist'])
-print(param)
-y = st.rayleigh.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
-ax1.plot(x, y, label='Rayleigh')
-results = st.ks_2samp(locM['dist'],y)
-print('KS-Test: ',results)
-#results = st.anderson_ksamp([locM['dist'],y])
-print('Anderson: ',results)
-y = st.rayleigh.cdf(x, *param[:-2], loc=param[-2], scale=param[-1])
-ax2.plot(x, y, label='Rayleigh')
-rayParam = param
+    # Perform fits and 
+    x = np.linspace(y.min(), y.max(), 10000)
+
+    param = st.rayleigh.fit(locM['dist'])
+    y = st.rayleigh.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
+    ax1.plot(x, y, label='Rayleigh')
+    results = st.ks_2samp(locM['dist'],y)
+    ks = results[0]
+
+    scale = param[-1]
+    loc = param[-2]
+
+    # Compute variance
+    rayVar = st.rayleigh.var(loc=loc,scale=scale)
+    distVar = locM['dist'].var()
 
 
-print('\nWeibull Distribution')
-param = st.weibull_max.fit(locM['dist'])
-print(param)
-y = st.weibull_max.pdf(x, *param[:-2], loc=param[-2], scale=param[-1])
-ax1.plot(x, y, label='Weibull Max')
-results = st.ks_2samp(locM['dist'],y)
-print('KS-Test: ',results)
-#results = st.anderson_ksamp([locM['dist'],y])
-print('Anderson: ',results)
-y = st.weibull_max.cdf(x, *param[:-2], loc=param[-2], scale=param[-1])
-ax2.plot(x, y, label='Weibull Max')
-weiParam = param
+    fout.write(form.format(a,numcells,scale,loc,rayVar,distVar,ks))
 
-
-
-#pd.DataFrame.hist(locM,column='dist',ax=ax,bins=numbins,color='r')
-ax1.set_xlabel('Distance from line')
-ax2.set_xlabel('Distance from line')
-
-plt.legend()
-s = 'vela2b-27_inflow_coherence.png'
-fig.savefig(s, bbox_inches='tight', dpi=300)
-
-
-# Compute variance
-rayVar = st.rayleigh.var(loc=rayParam[-2],scale=rayParam[-1])
-weiVar = st.weibull_max.var(c=weiParam[0], loc=weiParam[1],scale=weiParam[2])
-distVar = locM['dist'].var()
-
-print('Rayleigh Variance:     {0:.3f}'.format(rayVar))
-print('Weibull Variance:      {0:.3f}'.format(weiVar))
-print('Distribution Variance: {0:.3f}'.format(distVar))
-
-# Compute standard deviation
-rayStd = st.rayleigh.std(loc=rayParam[-2],scale=rayParam[-1])
-weiStd = st.weibull_max.std(c=weiParam[0], loc=weiParam[1],scale=weiParam[2])
-distStd = locM['dist'].std()
-
-print('Rayleigh Std Dev:     {0:.3f}'.format(rayStd))
-print('Weibull Std Dev:      {0:.3f}'.format(weiStd))
-print('Distribution Std Dev: {0:.3f}'.format(distStd))
-
-
+fout.close()
 
 
 

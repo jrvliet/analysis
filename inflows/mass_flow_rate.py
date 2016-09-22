849e186f0337c24859ed9331b6c4ed476cd22a31
Jacob Vander Vliet <jrvliet@gmail.com>
2016-09-22 14:17:32 -0600

    Fixed unit conversions issues

--- inflows/mass_flow_rate.py
+++ inflows/mass_flow_rate.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 mH = 1.6737e-24
 pc2cm = 3.086e18
+pc2km = 3.086e13
 mSun = 1.989e33
 
 
@@ -20,10 +21,10 @@ timeSteps = times['Time until next [Myr]']
 expns = np.arange(0.200,0.500,0.01)
 
 aM = []
+aR = []
 
 for i,a in enumerate(expns):
 
-    time = timeSteps.iloc[i]*1e6*3.154e7
 
 
     with open(rotmat.format(a), 'r') as f:
@@ -34,32 +35,46 @@ for i,a in enumerate(expns):
 
     df = pd.read_hdf(fname.format(a),'data')
 
+    # Calculate mass of each cell in units of Msun
     df['mass'] = df['density']*mH* (df['cell_size']*pc2cm)**3 / mSun 
     
+    # Calculate galactocentric distance in units of pc
     df['r'] = np.sqrt( df['x']**2 + df['y']**2 + df['z']**2 )
 
+    # Calculate radial velocity in units of km/s
     df['vr'] = (df['vx']*df['x'] + df['vy']*df['y'] + df['vz']*df['z']) / df['r']
 
-    df['range'] = df['vr']*time
-    
-    index = ( df['vr']*time < df['r'] )
+    # Calculate the time between this snapshot and the next in units of s
+    timeyr = timeSteps.iloc[i]*1e6
+    time = timeyr*3.154e7
 
+    # Calculate the range of each cell in the radial direction in units of pc
+    df['range'] = df['vr']*time/pc2km
+    
+    # Select out all cells that will reach the galaxy
+    index = ( df['vr']*time < df['r'] )
     accreting = df[index]
 
     accretingMass = accreting['mass'].sum()
+    accretingRate = accretingMass/timeyr
     
     aM.append(accretingMass)
-    print(a,accretingMass)
+    aR.append(accretingRate)
+    print(a,accretingMass,accretingRate)
 
 
 print(len(expns),len(aM))
 print(type(expns),type(aM))
-fig, ax = plt.subplots(1,1,figsize=(5,5))
+fig, (ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
 
-ax.plot(expns,aM)
-ax.set_xlabel('a')
-ax.set_ylabel('Accreting Mass')
-ax.set_yscale('log')
+ax1.plot(expns,aM)
+ax1.set_xlabel('a')
+ax1.set_ylabel('Accreting Mass [Msun]')
+ax1.set_yscale('log')
+
+ax2.plot(expns,aR)
+ax2.set_xlabel('a')
+ax2.set_ylabel('Accreting Rate [Msun/yr]')
 
 s = 'accretingMass.png'
 fig.savefig(s, bbox_inches='tight', dpi=300)

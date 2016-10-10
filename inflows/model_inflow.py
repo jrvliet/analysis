c4e144606cd3ef61e67559b30efa1780ca0a8bc2
Jacob Vander Vliet <jrvliet@gmail.com>
2016-10-09 21:41:46 -0600

    Spatial model of the inflow

---
+++ inflows/model_inflow.py
@@ -0,0 +1,85 @@
+
+# In[11]:
+
+from __future__ import print_function
+import matplotlib as mp
+mp.use('Agg')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.linalg as sl
+import scipy.stats as st
+
+
+
+
+numpoints = 1 000 000
+zmin = 0
+zmax = 3
+xmean, ymean = 0, 0
+xsigma, ysigma = 2, 2
+
+
+# In[25]:
+
+rayVar, datVar, ratio = [], [], []
+
+nums = range(1,8)
+for num in nums:
+    numpoints = 10**num
+    x = np.random.normal(loc=xmean, scale=xsigma, size=numpoints)
+    y = np.random.normal(loc=ymean, scale=ysigma, size=numpoints)
+    z = np.random.uniform(low=zmin, high=zmax, size=numpoints)
+
+
+    # Make datafram
+    df = pd.DataFrame({'x':x, 'y':y, 'z':z})
+
+    # Fit line of best fit
+    locM = df - df.mean()
+    covar = locM.cov()
+    (u,s,v) = sl.svd(covar)
+    u0 = u[0,0]
+    u1 = u[1,0]
+    u2 = u[2,0]
+
+    # Get distance ot this line for each cell
+    locM['a0'] = u2*locM['y'] - u1*locM['z']
+    locM['a1'] = u0*locM['z'] - u2*locM['x']
+    locM['a2'] = u1*locM['x'] - u0*locM['y']
+    locM['dist'] = np.sqrt(locM['a0']**2 + locM['a1']**2 + locM['a2']**2)
+
+    # Fit rayleigh to distance distribution
+    param = st.rayleigh.fit(locM['dist'])
+    loc = param[0]
+    scale = param[1]
+
+    # Get variance
+    rv = st.rayleigh.var(loc=loc,scale=scale)
+    dv = locM['dist'].var()
+    rayVar.append(rv)
+    datVar.append(dv)
+    ratio.append(rv/dv)
+
+    #xfit = np.linspace(locM['dist'].min(),locM['dist'].max())
+    #yfit = st.rayleigh.pdf(xfit, loc=loc, scale=scale)
+    #fig,ax = plt.subplots(1,1,figsize=(5,5))
+    #ax.hist(locM['dist'],bins=500,histtype='step',normed=True)
+    #ax.plot(xfit,yfit)
+
+
+fig, (ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
+ax1.plot(nums,rayVar,label='Fit')
+ax1.plot(nums,datVar,label='Data')
+ax1.set_xlabel('log Number of Points')
+ax1.set_ylabel('Variance')
+
+ax2.plot(nums,ratio)
+ax2.set_xlabel('log Number of Points')
+ax2.set_ylabel('Fit Variance / Data Variance')
+
+
+fig.tight_layout()
+s = 'model_inflow_variance.png'
+fig.savefig(s,bbox_inches='tight',dpi=300)
+

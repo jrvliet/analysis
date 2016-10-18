5dd4e4574cbdeae3710e4307414c4e1979a21e63
Jacob Vander Vliet <jrvliet@gmail.com>
2016-10-17 20:58:54 -0600

    Added simple plots with fewer lines

--- inflows/density_cuts_analysis.py
+++ inflows/density_cuts_analysis.py
@@ -13,7 +13,9 @@ import matplotlib as mp
 
 print(mp.matplotlib_fname())
 
-
+# Read in times
+times = pd.read_csv('lookback_time.csv')
+times = times.set_index('a')
 
 # Read in data
 loc = '/home/jacob/research/code/analysis/inflows/'
@@ -29,11 +31,16 @@ df = df[df['numCells']>numCellLim]
 
 # Calculate combined standard deviations
 df['locStd'] = np.sqrt((df['xRotStd']**2 + df['yRotStd']**2)/df['zRotStd']**2)
-df['totStd'] = np.sqrt(df['locStd']**2 + df['speedStd']**2)
+#df['totStd'] = np.sqrt(df['locStd']**2 + df['speedStd']**2)
 
+print(np.log10(df['vStatMean'].min()))
+print(np.log10(df['vStatStd'].min()))
 
 groups = df.groupby(['loN','hiN'])
 
+simples = [(-5.5,-4.5),(-4.5,-3.5),(-3.5,-2.5)]
+simples = [(-5.5,-5.0),(-5.0,-4.5),(-4.5,-4.0),
+           (-4.0,-3.5),(-3.5,-3.0),(-3.0,-2.5)]
 
 # Loop over various cuts of the number of cells 
 cellLims = np.arange(4.0,6,0.5)
@@ -43,19 +50,26 @@ cellLims = [4.0]
 fields = ['speedStd','stdDev','valongStd','vperpStd','locStd',
           'snIIStd','snIaStd','snIImean','snIamean',
           'rMean','rStd','nHmean','nHStd','tMean','tStd',
-          'rMeanMod','speedMean','valongMean','vperpMean', 'numCells']
+          'rMeanMod','speedMean','valongMean','vperpMean', 'numCells',
+          'vStatMean','vStatStd','vrMean','vrStd',
+          'rRotMean','rRotStd','vrRotMean','vrRotStd']
 
 # Plotting options
 logfields = ['snIIStd','snIaStd','snIImean','snIamean',
-             'nHmean','nHStd','nHmean','tMean','tStd']
+             'nHmean','nHStd','nHmean','tMean','tStd',
+             'vStatMean','vStatStd']
 upperlims = [80, 60, 90, 70, 4.00, 
              1e-1, 1e-2, 1e-1, 1e-2, 
              500, 250 , 1e-1, 1e-1, 1e5, 10**4.5,
-             6, 300, 250, 175, 700000]
+             6, 300, 250, 175, 700000,
+             1e4, 1e6, 200, 200,
+             500, 250, 200, 200]
 lowerlims = [0, 0, 0, 0, 0.10, 
              1e-5, 1e-8, 1e-5, 1e-8, 
              1, 1, 1e-6, 1e-6, 1e4, 1e3,
-             0, 30, 0, 10, 0]
+             0, 30, 0, 10, 0,
+             0.1, 0.1, -300, 0,
+             0, 0, -300, 0]
 lines = ['-','--','-.']
 markers = ['o','s','^','*','x']
 #dfFull = df.copy()
@@ -72,39 +86,101 @@ for numCellLim in cellLims:
     # Loop over the fields and plot them
     for i in range(len(fields)):
         field = fields[i]
-        fig,ax = plt.subplots(1,1,figsize=(10,10))
+
+        figAll,axAll = plt.subplots(1,1,figsize=(10,10))
+        figSimp,axSimp = plt.subplots(1,1,figsize=(10,10))
+        figAll2,axAll2 = plt.subplots(1,1,figsize=(10,10))
+        figSimp2,axSimp2 = plt.subplots(1,1,figsize=(10,10))
+
         linecycler = cycle(lines)
         markercycler = cycle(markers)
         for key, group in groups:
-            ax.plot(group['a'],group[field],label=key,
+
+            t = []
+            for j in range(len(group[field])):
+                t.append(times['age [Gyr]'].ix[group['a'].ix[j]])
+            
+            print(len(t),len(group[field]))
+            
+            axAll.plot(group['a'],group[field],label=key,
                     linestyle=next(linecycler),
                     marker=next(markercycler))
-        ax.set_xlabel('a')
-        ax.set_ylabel(field)
-        ax.legend(loc='upper left',ncol=5,labelspacing=0,frameon=True)
-        upperlim, lowerlim = ax.get_ylim()
-        ax.set_ylim([lowerlims[i],upperlims[i]])
-        ax.set_xlim([0.2,0.5])
+            print(len(times['age [Gyr]']),len(group[field]))
+            axAll2.plot(times['age [Gyr]'],group[field],label=key,
+                    linestyle=next(linecycler),
+                    marker=next(markercycler))
+            if key in simples:
+                axSimp.plot(group['a'],group[field],
+                        label=key,
+                        linestyle='solid',
+                        marker=next(markercycler))
+                axSimp2.plot(times['age [Gyr]'],group[field],
+                        label=key,
+                        linestyle='solid',
+                        marker=next(markercycler))
+        
+        for ax in [axAll,axSimp]:
+            ax.set_xlabel('a')
+            ax.set_ylabel(field)
+            ax.set_ylim([lowerlims[i],upperlims[i]])
+            ax.set_xlim([0.2,0.5])
 
-        # Plot four vertical lines. 
-        #   First two are red and denote the start and end of the
-        #   major merger
-        #   Second two are green are denote the start and end of the
-        #   "green zone", where some properties seem to diverge
-        #   for currently unknown reasons
-        ax.vlines(0.29,lowerlims[i],upperlims[i],colors='r',
-                  linestyles='dashed',linewidth=2)
-        ax.vlines(0.32,lowerlims[i],upperlims[i],colors='r',
-                  linestyles='dashed',linewidth=2)
-        ax.vlines(0.35,lowerlims[i],upperlims[i],colors='g',
-                  linestyles='dashed',linewidth=2)
-        ax.vlines(0.45,lowerlims[i],upperlims[i],colors='g',
-                  linestyles='dashed',linewidth=2)
-        if field in logfields:
-            ax.set_yscale('log')
+            # Plot four vertical lines. 
+            #   First two are red and denote the start and end of the
+            #   major merger
+            #   Second two are green are denote the start and end of the
+            #   "green zone", where some properties seem to diverge
+            #   for currently unknown reasons
+            ax.vlines(0.29,lowerlims[i],upperlims[i],colors='r',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(0.32,lowerlims[i],upperlims[i],colors='r',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(0.35,lowerlims[i],upperlims[i],colors='g',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(0.45,lowerlims[i],upperlims[i],colors='g',
+                      linestyles='dashed',linewidth=2)
+            if field in logfields:
+                ax.set_yscale('log')
+        for ax in [axAll2,axSimp2]:
+            ax.set_xlabel('Age [Gyr]')
+            ax.set_ylabel(field)
+            ax.set_ylim([lowerlims[i],upperlims[i]])
+            ax.set_xlim([1.5,6.0])
+
+            # Plot four vertical lines. 
+            #   First two are red and denote the start and end of the
+            #   major merger
+            #   Second two are green are denote the start and end of the
+            #   "green zone", where some properties seem to diverge
+            #   for currently unknown reasons
+            ax.vlines(2.68,lowerlims[i],upperlims[i],colors='r',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(5.07,lowerlims[i],upperlims[i],colors='r',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(2.68,lowerlims[i],upperlims[i],colors='g',
+                      linestyles='dashed',linewidth=2)
+            ax.vlines(5.07,lowerlims[i],upperlims[i],colors='g',
+                      linestyles='dashed',linewidth=2)
+            if field in logfields:
+                ax.set_yscale('log')
+
+        axAll.legend(loc='upper left',ncol=5,labelspacing=0,frameon=True)
+        axSimp.legend(loc='upper left',ncol=1,labelspacing=0,frameon=True)
+        axAll2.legend(loc='upper left',ncol=5,labelspacing=0,frameon=True)
+        axSimp2.legend(loc='upper left',ncol=1,labelspacing=0,frameon=True)
         s = './denseCuts/density_cuts_evolution_{0:s}_{1:.1f}.png'.format(field,numCellLim)
-        fig.savefig(s,bbox_inches='tight',dpi=300)
-        plt.close(fig)
+        figAll.savefig(s,bbox_inches='tight',dpi=300)
+        s = './denseCuts/density_cuts_evolution_{0:s}_{1:.1f}_time.png'.format(field,numCellLim)
+        figAll2.savefig(s,bbox_inches='tight',dpi=300)
+        s = './denseCuts/simple_density_cuts_evolution_{0:s}_{1:.1f}.png'.format(field,numCellLim)
+        figSimp.savefig(s,bbox_inches='tight',dpi=300)
+        s = './denseCuts/simple_density_cuts_evolution_{0:s}_{1:.1f}_time.png'.format(field,numCellLim)
+        figSimp2.savefig(s,bbox_inches='tight',dpi=300)
+
+        plt.close(figAll)
+        plt.close(figSimp)
+        plt.close(figAll2)
+        plt.close(figSimp2)
 
 
 

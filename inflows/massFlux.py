843d4d3cf0a145a4bcb019e854fe83b9facf485c
Jacob Vander Vliet <jrvliet@gmail.com>
2016-11-10 16:28:22 -0700

    Tested massFlux; now seperates inflows/outflows

--- inflows/massFlux.py
+++ inflows/massFlux.py
@@ -1,22 +1,18 @@
+# coding: utf-8 
+from __future__ import print_function 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+pd.options.mode.chained_assignment = None 
 
-# coding: utf-8
+kpc2km = 3.086e16 
+pc2cm = 3.086e18 
+mH = 1.6737e-24 
+mSun = 1.989e33 
+s2yr = 3.154e7 
 
-from __future__ import print_function
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib inline')
-pd.options.mode.chained_assignment = None
-
-kpc2km = 3.086e16
-pc2cm = 3.086e18
-mH = 1.6737e-24
-mSun = 1.989e33
-s2yr = 3.154e7
-
-
-dataloc = '/home/jacob/research/velas/vela2b/vela27/'
-filename = 'a{0:.3f}/vela2b-27_GZa{0:.3f}.rot.h5'
+dataloc = '/home/jacob/research/velas/vela2b/vela27/' 
+filename = 'a{0:.3f}/vela2b-27_GZa{0:.3f}.rot.h5' 
 rotname = 'a{0:.3f}/rotmat_a{0:.3f}.txt'
 
 expns = np.arange(0.200,0.500,0.01)
@@ -34,28 +30,57 @@ for a in expns:
         rvir = float(f.readline().split()[3])
 
     df['rMod'] = df['r']/rvir
-    df['vr'] = np.sqrt(df['vxRot']**2 + df['vyRot']**2 + df['vzRot']**2)
+    #df['rkm'] = df['r']*kpc2km
+    df['vr'] = (df['vxRot']*df['xRot'] + df['vyRot']*df['yRot'] +
+                        df['vzRot']*df['zRot']) / df['r']
     df['vr'] = df['vr']/(kpc2km*rvir)*s2yr
     df['mass'] = df['density']*mH*(df['cell_size']*pc2cm)**3 / mSun
     df['mass'].describe()
-    get_ipython().magic(u'timeit')
-    shellFlux = np.zeros(len(rPoints))
-    print(rPoints)
+
+    shellIn = np.zeros(len(rPoints))
+    shellOut = np.zeros(len(rPoints))
+    
     for i,dist in enumerate(rPoints):
         shellIndex = (df['rMod']<dist+dr) & (df['rMod']>dist-dr)
-        shell = df[shellIndex]
-        shellFlux[i] = (shell['mass']*shell['vr'] / dr).sum()
-    fig,ax = plt.subplots(1,1,figsize=(5,5))
-    ax.plot(rPoints,shellFlux)
-    ax.set_xlabel('Distance from Galaxy [Rvir]')
-    ax.set_ylabel('Mass Flux [Msun/yr]')
-    ax.set_title('a={0:.3f}, z={0:.3f}'.format(a,1./a-1))
+        inIndex = (df['vr']<0)
+        outIndex = (df['vr']>0)
+
+#        print(len(inIndex))
+#        print(len(outIndex))
+        shellInfall = df[shellIndex & inIndex]
+        shellOutflow = df[shellIndex & outIndex]
+        
+        shellIn[i] = (shellInfall['mass']*shellInfall['vr'] / dr).sum()
+        shellOut[i] = (shellOutflow['mass']*shellOutflow['vr'] / dr).sum()
     
-    print(shellFlux.max())
+    print('In:  {0:.3f} - {1:.3f}'.format(shellIn.min(),shellIn.max()), end='\t')
+    print('Out: {0:.3f} - {1:.3f}'.format(shellOut.min(),shellOut.max()))
+
+    #fig,(ax1,ax2) = plt.subplots(1,2,figsize=(10,5))
+    #ax1.plot(rPoints,np.abs(shellIn))
+    #ax2.plot(rPoints,shellOut)
+
+    fig,ax = plt.subplots(1,1,figsize=(5,5))
+    ax.plot(rPoints,np.abs(shellIn),color='blue',label='Inflow')
+    ax.plot(rPoints,shellOut,color='red',label='Outflow')
+    
+    #for ax in [ax1,ax2]:
+    for ax in [ax]:
+        ax.set_xlabel('Distance from Galaxy [Rvir]')
+        ax.set_ylabel('Mass Flux [Msun/yr]')
+        ax.set_xlim([0,5])
+        ax.set_ylim([0,200])
+
+    #ax1.set_title('Inflow')
+    #ax2.set_title('Outflow')
+    #fig.suptitle('a={0:.3f}, z={1:.3f}'.format(a,1./a-1))
+    #fig.tight_layout()
+    
+    ax.set_title('a={0:.3f}, z={1:.3f}'.format(a,1./a-1))
+    ax.legend(loc='upper right')
 
     s = 'vela2b-27_massFlux_a{0:.3f}.png'.format(a)
     fig.savefig(s,bbox_inches='tight',dpi=300)
     plt.close(fig)
-
 
 

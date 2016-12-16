4776fb19047433f60b9a1dbf906b7554321dbc96
Jacob Vander Vliet <jrvliet@gmail.com>
2016-12-16 16:54:47 -0700

    Increased bin size on metal mass histograms

--- inflows/metalMassHist.py
+++ inflows/metalMassHist.py
@@ -7,16 +7,18 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib inline')
 import scipy.stats as st
 pd.options.mode.chained_assignment = None
 import matplotlib.ticker as ticker
 
 
+def fmt(x,pos):
+    return '{0:.1f}'.format(x)
+
 # In[16]:
 
 def mkHist(ax,x,y,z,stat,xlabel,ylabel,lox,hix,loy,hiy):
-    numbins = 50
+    numbins = 500
     #stat = 'count'
     binrange = [[lox,hix],[loy,hiy]]
     h,xedges,yedges,binnumber = st.binned_statistic_2d(x,y,z,
@@ -28,12 +30,13 @@ def mkHist(ax,x,y,z,stat,xlabel,ylabel,lox,hix,loy,hiy):
     h = np.ma.masked_where(h==0,h)
     h = np.log10(h)
 
-    mesh = ax.pcolormesh(xedges,yedges,h)
+    mesh = ax.pcolormesh(xedges,yedges,h,vmin=0)
     ax.set_xlim(binrange[0])
     ax.set_ylim(binrange[1])
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    cbar = plt.colorbar(mesh,ax=ax,use_gridspec=True)
+    cbar = plt.colorbar(mesh,ax=ax,use_gridspec=True,
+                       format=ticker.FuncFormatter(fmt) )
     return h,xedges,yedges,binnumber
 
 
@@ -52,7 +55,6 @@ def cellMass(n,l):
 
 loc = '/home/jacob/research/velas/vela2b/vela27/'
 filename = 'a{0:.3f}/vela2b-27_GZa{0:.3f}.rot.h5'
-expns = [0.490]
 expns = np.arange(0.200,0.550,0.01)
 
 
@@ -61,10 +63,13 @@ expns = np.arange(0.200,0.550,0.01)
 loT,hiT = 3.5,6.5
 loN,hiN = -5.5,-2.5
 
+header = 'a nMax tMax'.split()
+maxMetal = np.zeros((len(expns),len(header)))
+maxMetal = pd.DataFrame(maxMetal,columns=header)
 
 # In[18]:
 
-for a in expns:
+for i,a in enumerate(expns):
     print(a)
     fname = loc+filename.format(a)
     df = pd.read_hdf(fname,'data')
@@ -77,14 +82,27 @@ for a in expns:
     fil['metalMass'] = fil['mass']*(fil['SNII']+fil['SNIa'])
     
     fig,ax = plt.subplots(1,1,figsize=(5,5))
-    mkHist(ax,np.log10(fil['density']),np.log10(fil['temperature']),fil['metalMass'],
-            'sum','Density','Temperature',loN,hiN,loT,hiT)
+    h,xedges,yedges,binnumber = mkHist(ax,np.log10(fil['density']),
+                                np.log10(fil['temperature']),fil['metalMass'],
+                                'sum','Density','Temperature',loN,hiN,loT,hiT)
+    ax.set_title('{0:.3f}'.format(a))
     s = 'metalMassHist_a{0:.3f}.png'.format(a)
     fig.savefig(s,bbox_inches='tight',dpi=300)
     plt.close(fig)
 
+    # Get the location of the maximum metal mass
+    tMaxInd,nMaxInd = np.unravel_index(h.argmax(), h.shape)
+    tMax = yedges[tMaxInd]
+    nMax = xedges[nMaxInd]
 
-# In[ ]:
+    # Record these values
+    maxMetal['a'].ix[i] = a
+    maxMetal['nMax'].ix[i] = nMax
+    maxMetal['tMax'].ix[i] = tMax
+
+    
+hdfFile = 'maxMetalPhase.h5'
+maxMetal.to_hdf(hdfFile,'data',mode='w')
 
 
 

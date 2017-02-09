993346ca5d7f6e2805d80efc158d053ad0a8d0a3
Jacob Vander Vliet <jvander@g2.hpc.swin.edu.au>
2017-02-10 06:57:55 +1100

    Fion tested

--- inflows/fion.py
+++ inflows/fion.py
@@ -16,14 +16,15 @@ boltz = 1.3807e-16
 cm2km = 1e5
 
 loc = '/home/jacob/research/velas/vela2b/vela27/'
+loc = '/mnt/cluster/abs/cgm/vela2b/vela27/'
 ionfilename = 'a{0:.3f}/vela2b-27_GZa{0:.3f}.{1:s}.h5'
 boxfilename = 'a{0:.3f}/vela2b-27_GZa{0:.3f}.rot.h5'
 ions = 'HI MgII CIV OVI'.split()
 expns = np.arange(0.200,0.500,0.01)
-expns = [0.400]
+#expns = [0.400]
 
 lowestT,highestT = 3.5,6.5
-lowestN,highestN = -2.5,-5.5
+lowestN,highestN = -5.5,-2.5
 numTbins,numNbins = 3,3
 tBins = np.linspace(lowestT,highestT,numTbins+1)
 nBins = np.linspace(lowestN,highestN,numNbins+1)
@@ -31,7 +32,7 @@ nBins = np.linspace(lowestN,highestN,numNbins+1)
 tempHeader = 'cool warm hot'.split()
 denseHeader = 'diffuse mid dense'.split()
 fields = 'mean std'.split()
-header = [tempHeader,denseHeader,fields]
+header = [tempHeader,denseHeader,ions,fields]
 header = pd.MultiIndex.from_product(header)
 expnLabels = ['a{0:d}'.format(int(a*1000)) for a in expns]
 results = pd.DataFrame(columns=header,index=expnLabels)
@@ -44,7 +45,6 @@ for i,a in enumerate(expns):
 
     spaceInds = box['theta']<80
 
-    print(spaceInds.sum())
     for ion in ions:
 
         print(ion)
@@ -57,32 +57,26 @@ for i,a in enumerate(expns):
             tempLabel = tempHeader[j]
             loT,hiT = tBins[j],tBins[j+1]
             tempBins = (df['temperature']>10**loT) & (df['temperature']<10**hiT)
-            print(tempBins.sum())
             
             for k in range(numNbins):
 
                 denseLabel = denseHeader[k]
                 loN,hiN = nBins[k],nBins[k+1]
-                denseBins1 = (df['nH']<10**loN) 
-                denseBins2 = (df['nH']>10**hiN)
-                denseBins = denseBins1 & denseBins2
-                print(denseBins1.sum())
-                print(denseBins2.sum())
-                print(loN,hiN)
-                print(df['nH'].describe())
-                print(denseBins.sum())
-
+                denseBins = (df['nH']>10**loN) & (df['nH']<10**hiN)
                 fil = df[spaceInds & tempBins & denseBins]
-                print(len(fil))
 
                 fil['mass'] = (fil['nH']*u2g*pc2cm**3/g2M)*fil['cell_size']**3
     
-                results[tempLabel,denseLabel,fields[0]].ix[i] = np.average(fil['fIon'],
+                results[tempLabel,denseLabel,ion,fields[0]].ix[i] = np.average(fil['fIon'],
                                                                     weights=fil['mass'])
-                results[tempLabel,denseLabel,fields[1]].ix[i] = fil['fIon'].std()
+                results[tempLabel,denseLabel,ion,fields[1]].ix[i] = fil['fIon'].std()
 
 s = 'fIon.h5'
-results.to_hdf(s,'data',mode='w')
+results.to_hdf(s,'data',mode='w',format='fixed')
+s = 'fIon_utf.csv'
+results.to_csv(s,encoding='utf-8')
+s = 'fIon_ascii.csv'
+results.to_csv(s,encoding='ascii')
                 
                 
                  

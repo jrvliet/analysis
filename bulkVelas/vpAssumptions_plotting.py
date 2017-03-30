51c6aa57473e39ab29b595dfa2953cf2f5a13b14
Jacob Vander Vliet <jrvliet@gmail.com>
2017-03-29 23:22:53 -0600

    Testing of plotting code

--- bulkVelas/vpAssumptions_plotting.py
+++ bulkVelas/vpAssumptions_plotting.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='white')
+import sys
 
 
 loc = '/home/jacob/research/code/analysis/bulkVelas/'
@@ -14,7 +15,7 @@ fname = loc+filename
 df = pd.read_hdf(fname,'data')
 
 galNums = range(21,30)
-expns = np.arange(0.200,0.500,0.01)
+expns = np.arange(0.200,0.550,0.01)
 expnLabels = ['a{0:d}'.format(int(a*1000)) for a in expns]
 ions = 'HI MgII CIV OVI'.split()
 
@@ -24,22 +25,29 @@ for prop in props:
     fields.append('{0:s}.mean'.format(prop))
     fields.append('{0:s}.std'.format(prop))
 
+logFields = 't n Z'.split()
 
 
 for field in fields:
+    print(field)
 
     fig,axes = plt.subplots(2,2,figsize=(10,10))
     
     for ion,ax in zip(ions,axes.flatten()):
-
         for galNum in galNums:
-
-            y = df[ion,field].ix[galNum]
-            ax.plot(expns,y,label=galNum)
+            x = expns
+            y = df[ion,field].ix[galNum].astype(float)
+            if field[0] in logFields:
+                y = np.log10(y)
+            #print(len(x),len(y))
+            #print(x)
+            #print(y)
+            ax.plot(x,y,label=galNum)
 
         ax.set_xlabel('Expn')
         ax.set_ylabel(field)
         ax.set_title(ion)
+    axes[0,0].legend(loc='best')
     fig.tight_layout()
     s = 'vpAssumptions_{0:s}.png'.format(field.replace('.','_'))
     fig.savefig(s,bbox_inches='tight',dpi=300)

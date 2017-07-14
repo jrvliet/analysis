0e7409f0964fa0a2416aba2e51fb55f65c37c761
Jacob Vander Vliet <jvander@g2.hpc.swin.edu.au>
2017-07-15 08:56:13 +1000

    Mean profile now generated for different orientations

--- ovi/mean_profile.py
+++ ovi/mean_profile.py
@@ -25,6 +25,7 @@ subloc = 'vela{0:d}/a0.490/i90/OVI/'
 
 sysabsName = 'vela2b-{0:d}.OVI.los{1:04d}.sysabs'
 specName = 'vela2b-{0:d}.OVI.los{1:04d}.OVI{2:d}.spec'
+linesHeader = 'losnum impoact phi incline'.split()
 
 galNums = range(21,30)
 losnums = range(1,1000)
@@ -34,15 +35,20 @@ azCutoff = 30.
 
 for tran in trans:
     print('Transition: {0:d}'.format(tran))
-    flux = []
-    sigma = []
-    lengths = []
+    majorFlux,minorFlux = [],[]
+    majorSigma,minorSigma = [],[]
     i = 0
 
     for galNum in galNums:
 
         print('\tGalaxy: {0:d}'.format(galNum))
         loc = baseloc+subloc.format(galNum)
+        lines = loc+'lines.info'
+        try:
+            lines = pd.read_csv(lines,sep='\s+',skiprows=2,names=linesHeader)
+            lines['az'] = lines['phi'].apply(azimuthal)
+        except IOError:
+            pass
 
         for losnum in losnums:
 
@@ -53,34 +59,43 @@ for tran in trans:
                 fi = open(syName,'r')
                 fi.close()
                 vel,f,s = np.loadtxt(spName,usecols=(1,2,3),unpack=True)
+            
+                az = lines['az'].iloc[losnum-1]
+                if az<azCutoff:
+                    majorFlux.append(f)
+                    majorSigma.append(s)
+                else:
+                    minorFlux.append(f)
+                    minorSigma.append(s)
+                    
             except IOError:
                 pass
 
-            if len(f)<900 and losnum==1:
-                print(galNum,losnum,len(f))
-            flux.append(f)
-            sigma.append(s)
-            lengths.append(len(f))
             i += 1
 
-    flux = np.asarray(flux)
-    sigma = np.asarray(sigma)
-    top = np.sum(flux/sigma**2,axis=0)
-    bot = np.sum(sigma**(-2),axis=0)
-    y = top/bot
-    yerr = 1./np.sqrt(bot**2)
-    #y = np.mean(flux,axis=0)
-    #yerr = np.std(flux,axis=0)/np.sqrt(tot.shape[0])
+    
+    for flux,sigma,name in zip([majorFlux,minorFlux],
+                               [majorSigma,minorSigma],
+                               ['Major','Minor']):
 
-    fig,ax = plt.subplots(1,1,figsize=(5,5))
-    ax.errorbar(vel,y,yerr=yerr)
-    ax.set_xlabel('Velocity [km/s]')
-    ax.set_ylabel('Flux')
-    ax.set_ylim([0,1.2])
+        flux = np.asarray(flux)
+        sigma = np.asarray(sigma)
+        top = np.sum(flux/sigma**2,axis=0)
+        bot = np.sum(sigma**(-2),axis=0)
+        y = top/bot
+        yerr = 1./np.sqrt(bot**2)
+        #y = np.mean(flux,axis=0)
+        #yerr = np.std(flux,axis=0)/np.sqrt(tot.shape[0])
 
-    fig.savefig('meanOVI{0:d}profile.png'.format(tran),
-                bbox_inches='tight',dpi=300)
-    plt.close()
+        fig,ax = plt.subplots(1,1,figsize=(5,5))
+        ax.errorbar(vel,y,yerr=yerr)
+        ax.set_xlabel('Velocity [km/s]')
+        ax.set_ylabel('Flux')
+        ax.set_ylim([0,1.2])
+
+        fig.savefig('meanOVI{0:d}_{1:s}_profile.png'.format(tran,name),
+                    bbox_inches='tight',dpi=300)
+        plt.close()
             
             
             
